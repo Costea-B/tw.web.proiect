0256fe625484ccd04bbc409e7ecaf1615a71c9bf
Costea-B <155655464+Costea-B@users.noreply.github.com>
2024-02-28 22:21:26 +0200

    build(create class library, add new bundles scripts)

--- WebApplication1/App_Start/BundelConfig.cs
+++ WebApplication1/App_Start/BundelConfig.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -20,6 +19,50 @@ namespace WebApplication1.App_Start
                // CSS-ul aplicației
                bundles.Add(new StyleBundle("~/bundles/app/css").Include("~/Templaid/css/icons.min.css", "~/Templaid/css/app.min.css")
                     .Include("~/Content/font-awesome.min.css", new CssRewriteUrlTransform()));
+
+               //Vndor js
+               bundles.Add(new ScriptBundle("~/vendor/js").Include(
+                   "~/Templaid/js/vendor.min.js"
+                   ));
+
+               // App js
+               bundles.Add(new ScriptBundle("~/app/js").Include(
+                  "~/Templaid/js/app.min.js"
+                  ));
+
+               //Index js
+
+               bundles.Add(new ScriptBundle("~/index/js").Include(
+                    "~/Templaid/libs/apexcharts/apexcharts.min.js",
+                    "~/Templaid/libs/peity/jquery.peity.min.js",
+                    "~/ Templaid/libs/jquery-sparkline/jquery.sparkline.min.js",
+                    "~/Templaid/js/pages/dashboard-1.init.js"
+                    ));
+
+               //Deshbord2 js
+               bundles.Add(new ScriptBundle("~/dashbord/js").Include(
+                    "~/Templaid/libs/jquery-knob/jquery.knob.min.js",
+                    "~/Templaid/libs/jquery-sparkline/jquery.sparkline.min.js",
+                    "~/ Templaid/libs/jquery-vectormap/jquery-jvectormap-1.2.2.min.js",
+                    "~/ Templaid/libs/jquery-vectormap/jquery-jvectormap-world-mill-en.js",
+                    "~/ Templaid/libs/peity/jquery.peity.min.js",
+                    "~/Templaid/js/pages/dashboard-2.init.js"
+                    ));
+
+               //Calendar js
+               bundles.Add(new ScriptBundle("~/calendar/js").Include(
+                    "~/Templaid/libs/moment/moment.min.js",
+                    "~/Templaid/libs/jquery-ui/jquery-ui.min.js",
+                    "~/ Templaid/libs/fullcalendar/fullcalendar.min.js",
+                    "~/ Templaid/js/pages/calendar.init.js"
+                    ));
+
+               //Companies js
+               bundles.Add(new ScriptBundle("~/companies/js").Include(
+                    "~/Templaid/libs/jquery-sparkline/jquery.sparkline.min.js",
+                    "~/Templaid/libs/custombox/custombox.min.js",
+                    "~/ Templaid/js/pages/companies.js"
+                    ));
           }
      }
 }
