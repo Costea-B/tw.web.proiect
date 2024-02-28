0dd0cdb49e650f54a672123ce285363a45807833
Krosh1010 <157153398+Krosh1010@users.noreply.github.com>
2024-02-28 23:32:40 +0200

    S-a integrat pagina Pages_Login Pages_Register Pages_Lock_Screen

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -6,40 +6,52 @@ using System.Web.Mvc;
 
 namespace WebApplication1.Controllers
 {
-    public class HomeController : Controller
-    {
-        // GET: Home
-        public ActionResult Index()
-        {
-            return View();
-        }
-        public ActionResult Dashboard2()
+     public class HomeController : Controller
+     {
+          // GET: Home
+          public ActionResult Index()
           {
                return View();
           }
-        public ActionResult Charts()
+          public ActionResult Dashboard2()
           {
                return View();
           }
-        public ActionResult Calendar()
+          public ActionResult Charts()
           {
-            return View();
+               return View();
           }
-        public ActionResult Companies()
-        {
-            return View();
-        }
-        public ActionResult FileManager()
-        {
-            return View();
-        }
-        public ActionResult Tickets()
-        {
-            return View();
-        }
-        public ActionResult TeamMembers()
-        {
-            return View();
-        }
-    }
+          public ActionResult Calendar()
+          {
+               return View();
+          }
+          public ActionResult Companies()
+          {
+               return View();
+          }
+          public ActionResult FileManager()
+          {
+               return View();
+          }
+          public ActionResult Tickets()
+          {
+               return View();
+          }
+          public ActionResult TeamMembers()
+          {
+               return View();
+          }
+          public ActionResult Pages_Login()
+          {
+               return View();
+          }
+          public ActionResult Pages_Register()
+          {
+               return View();
+          }
+          public ActionResult Pages_Lock_Screen()
+          {
+               return View();
+          }
+     }
 }
